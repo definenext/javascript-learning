fe5eb0f3db24f4fbea2c5eea997e44c9bd43fdab
abhishekbhartiind <imabhishekbharti12@gmail.com>
2022-05-25 12:48:26 +0530

    added callback func

--- callback.js
+++ callback.js
@@ -9,3 +9,18 @@ function callbackTester(callback) {
 }
 
 callbackTester(tryMe, "hello", "goodbye");
+
+//Currying
+/**
+ * It is advance technique of working with function.
+ * It is tranformation of function that translates a function from callback as
+ * f(a, b) into f(a)(b)
+ */
+
+function add(a) {
+  return function (b) {
+    return a + b;
+  };
+}
+
+console.log(add(5)(6));
